7259368c0aa895e9d3e93734bc8324883120f858
warrens2uab <122620756+warrens2uab@users.noreply.github.com>
2023-07-23 20:55:50 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -43,7 +43,37 @@ def purchase_success():
     return render_template("purchaseConfirmed.html",
         PageName = "Success",
     )
+# User Page
+@app.route ('/Users')
+def user_homepage():
+    render_template ('base.html')
+    return render_template("usersHomepage.html",
+        PageName = "User",
+    )
 
+# Employee Page
+@app.route ('/Employees')
+def employee_homepage():
+    render_template ('base.html')
+    return render_template("workersHomepage.html",
+        PageName = "Employee",
+    )
+
+# View Inventory
+@app.route ('/ViewInventory')
+def view_inventory():
+    render_template ('base.html')
+    return render_template("viewInventory.html",
+        PageName = "Inventory"
+    )
+
+# View Purchases
+@app.route ('/ViewPurchases')
+def view_purchases():
+    render_template ('base.html')
+    return render_template("viewPurchases.html",
+        PageName = "Purchases"
+    )
 
 if __name__ == '__main__':
     app.run()
