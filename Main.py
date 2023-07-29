ae8cb073dde1ceb82b6a1f18e690368f49bc575a
warrens2uab <122620756+warrens2uab@users.noreply.github.com>
2023-07-29 18:07:47 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -43,20 +43,69 @@ def purchase_success():
     return render_template("purchaseConfirmed.html",
         PageName = "Success",
     )
+
 # User Page
 @app.route ('/Users')
 def user_homepage():
     render_template ('base.html')
-    return render_template("usersHomepage.html",
+    return render_template("userSignIn.html",
         PageName = "User",
+    )
+
+# User Sign Up
+@app.route('/UserSignUp')
+def user_signup():
+    render_template ('base.html')
+    return render_template("userSignUp.html",
+        PageName = "UserSignUp"        
+    )
+
+# User Sign In
+@app.route('/UserSignIn')
+def user_signin():
+    render_template ('base.html')
+    return render_template("usersHomepage.html",
+        PageName = "UserSignIn"
+    )
+
+# User Confirmed
+@app.route('/UserConfirmed')
+def user_confirmed():
+    render_template ('base.html')
+    return render_template("userConfirmed.html",
+        PageName = "UserConfirmed"
     )
 
 # Employee Page
 @app.route ('/Employees')
 def employee_homepage():
     render_template ('base.html')
-    return render_template("workersHomepage.html",
+    return render_template("workerSignIn.html",
         PageName = "Employee",
+    )
+
+# Employee Sign Up
+@app.route('/EmployeerSignUp')
+def employee_signup():
+    render_template ('base.html')
+    return render_template("workerSignUp.html",
+        PageName = "EmployeeSignUp"        
+    )
+
+# Employee Sign In
+@app.route('/EmployeeSignIn')
+def employee_signin():
+    render_template ('base.html')
+    return render_template("workersHomepage.html",
+        PageName = "EmployeeSignIn"
+    )
+
+# Employee Confirmed
+@app.route('/EmployeeConfirmed')
+def employee_confirmed():
+    render_template ('base.html')
+    return render_template("workerConfirmed.html",
+        PageName = "EmployeeConfirmed"
     )
 
 # View Inventory
